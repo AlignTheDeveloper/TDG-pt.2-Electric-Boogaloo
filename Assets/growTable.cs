6318180d8e9f2aed374289858ab8091fde8a7126
Killerconman311 <118021304+Killerconman311@users.noreply.github.com>
2023-04-28 17:20:07 -0600

    almost done
    almost finished with the expanding screen

--- Assets/growTable.cs
+++ Assets/growTable.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class growTable : MonoBehaviour
 {
+
     // Start is called before the first frame update
+    public float timer = 0f;
+    public float growTime = 10f;
+    public float maxSizeX = 10f;
+    public float maxSizeY = 0f;
+
+    public float TargetFOV = 160f;
+    public float Speed = .75f; //Will reach target value in 1sec.
+
+    public bool isMaxSize = false;
     void Start()
     {
-        
+        if (isMaxSize == false)
+        {
+            StartCoroutine(GrowTable());
+        }
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+
+       
     }
+
+    private IEnumerator GrowTable()
+    {
+
+
+        Vector2 startScale = transform.localScale;
+        Vector2 endScale = new Vector2(maxSizeX, maxSizeY);
+
+        do
+        {
+            transform.localScale = Vector2.Lerp(startScale, endScale, timer / growTime);
+            timer += Time.deltaTime;
+            yield return null;
+        }
+        while (timer < growTime);
+
+        isMaxSize = true;
+    }
+
+
+
+
+
 }
