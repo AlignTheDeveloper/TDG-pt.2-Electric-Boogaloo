1d3e9296066b50102869e971b3057d357ccaccf2
Grant <g.rantolson02@gmail.com>
2023-05-02 01:03:15 -0600

    Changed stuff
    blah

--- Assets/Scripts/TestCamMove.cs
+++ Assets/Scripts/TestCamMove.cs
@@ -74,7 +74,7 @@ public class TestCamMove : MonoBehaviour
         {
             Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             RaycastHit2D hit = Physics2D.Raycast(worldPoint, Vector2.zero);
-            int layerMask = LayerMask.GetMask("UI");
+            int layerMask = LayerMask.GetMask("ButtonLayer");
             Collider2D target = Physics2D.OverlapPoint(worldPoint, layerMask);
 
             if (target)
